8b8dd48f89ecb477d2a14cbd6b0eede85f6ca2d3
Fredzxcs <johnfredericktsabondo@iskolarngbayan.pup.edu.ph>
2024-09-25 19:35:42 -0800

    hard deleted admin

--- tikmeDine/tikmeReservation/static/js/reservation.js
+++
@@ -1,3 +0,0 @@
-        // Example JavaScript for scroll animations
-        ScrollReveal().reveal('.home', { duration: 2000, origin: 'bottom', distance: '50px' });
-        ScrollReveal().reveal('.reservation-container', { duration: 2000, origin: 'bottom', distance: '50px' });
--- tikmeDine/tikmeReservation/views.py
+++ tikmeDine/tikmeReservation/views.py
@@ -1,23 +1,8 @@
 # tikmeCustomer/views.py
 from django.shortcuts import render
 
-def home(request):
+def index(request):
     """View function for the home page."""
-    return render(request, 'mainCustomer/home.html')
+    return render(request, 'mainCustomer/index.html')
 
-def menu(request):
-    """View function for the menu page."""
-    return render(request, 'mainCustomer/menu.html')
-
-def reservation(request):
-    """View function for the reservation page."""
-    return render(request, 'mainCustomer/reservation.html')
-
-def faq(request):
-    """View function for the faq page."""
-    return render(request, 'mainCustomer/faq.html')
-
-def contact(request):
-    """View function for the contact page."""
-    return render(request, 'mainCustomer/contact.html')
 
