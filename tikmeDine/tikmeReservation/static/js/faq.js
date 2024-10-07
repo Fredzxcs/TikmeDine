4b60a1b712e5c9f70a6f9e5c78d9504d217ea0c1
Fredzxcs <johnfredericktsabondo@iskolarngbayan.pup.edu.ph>
2024-10-08 02:24:27 +0800

    v1

--- tikmeDine/tikmeReservation/static/js/faq.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.faq-item h3').forEach(item => {
-    item.addEventListener('click', () => {
-        const parent = item.parentElement;
-        parent.classList.toggle('active');
-    });
-});
